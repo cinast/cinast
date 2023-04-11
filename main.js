ea8f994a8daf64ff7783798182ce10f6aba4267e
cinast <126345646+cinast@users.noreply.github.com>
2023-04-11 12:32:08 +0000

    writing....

---
+++ main.js
@@ -0,0 +1,11 @@
+var lang = !(async ()=>{
+    return await fetch('./lang.json')
+    .then((response) => response.json())
+    .finally(() => console.log('lang.json:✓'))
+})
+async function changeLang(){
+    var words = document.getElementsByClassName('words')
+    words.forEach(i => {
+        words[i].innerHTML = lang[words[i].keyword]
+    });
+}
